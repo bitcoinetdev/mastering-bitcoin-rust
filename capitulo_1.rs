f06693124fe8a2d88025c2cc4b07f336310053ce
bitcoinet <78202555+bitcoinetdev@users.noreply.github.com>
2023-10-12 21:11:42 -0600

    Update capitulo_1.rs

--- capitulo_1.rs
+++ capitulo_1.rs
@@ -20,4 +20,28 @@ y bloques. Hay varios tipos de nodos:
 
 // Más secciones explicando composición de la red y flujo de transacciones
 
-// Próximamente más traducción del capítulo 1...
+// Validación de transacciones
+
+/*
+Las transacciones se transmiten a la red como "transacciones sin confirmar".
+Los nodos completos validan la transacción verificando:
+
+- Que la sintaxis sea correcta
+- Que la transacción no esté duplicada
+- Que las entradas sumen los valores de salida
+- Otras reglas de consenso
+
+Si la transacción es válida, se agrega al pool de memoria de transacciones 
+no confirmadas. De allí se agregará a un bloque.
+*/
+
+// Minería de bloques
+
+/*
+Los mineros compiten para calcular hashes con valor menor al objetivo. 
+El primero en lograrlo mina un bloque válido a la cadena. 
+
+El protocolo ajusta la dificultad para mantener 10min por bloque.
+*/
+
+// Continuará la traducción del Capítulo 1...
