5c6a7ffce00c9190f28b10382a39a3d1f93598bd
bitcoinet <78202555+bitcoinetdev@users.noreply.github.com>
2023-10-12 21:10:05 -0600

    Update capitulo_1.rs

--- capitulo_1.rs
+++ capitulo_1.rs
@@ -1,1 +1,23 @@
+// Capítulo 1: Introducción a Bitcoin
 
+/*
+Este capítulo proporciona una breve introducción a Bitcoin, comenzando 
+con un resumen de los principales componentes de la red Bitcoin. Luego
+explica brevemente cómo se realizan transacciones en la cadena de bloques, 
+cómo se validan y confirman.
+*/
+
+// Secciones de la red Bitcoin
+
+/*
+La red Bitcoin se compone de nodos que validan y retransmiten transacciones 
+y bloques. Hay varios tipos de nodos:
+
+- Nodos de minería - validan transacciones y bloques y generan hashes
+- Nodos completos - validan, almacenan blockchain completa y retransmiten
+- Nodos livianos - almacenan, validan y retransmiten, pero no toda la cadena
+*/
+
+// Más secciones explicando composición de la red y flujo de transacciones
+
+// Próximamente más traducción del capítulo 1...
