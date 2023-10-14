14974ee91750eef8145803600e164f63fc8c12a3
bitcoinet <78202555+bitcoinetdev@users.noreply.github.com>
2023-10-14 16:49:07 -0600

    Update capitulo_1.rs

--- capitulo_1.rs
+++ capitulo_1.rs
@@ -44,4 +44,48 @@ El primero en lograrlo mina un bloque válido a la cadena.
 El protocolo ajusta la dificultad para mantener 10min por bloque.
 */
 
-// Continuará la traducción del Capítulo 1...
+// Minado de un nuevo bloque
+
+/*
+Cuando se mina un nuevo bloque, éste se transmite a la red. Los nodos
+completos validan el bloque:
+
+- El hash previo debe ser correcto 
+- Debe cumplir la dificultad objetivo
+- Las transacciones deben ser válidas
+
+Si el bloque es válido, se agrega a la cadena y se avanza el puntero.
+Los nodos mantienen solo la cadena más larga.
+*/
+
+// Confirmaciones
+
+/*
+Las transacciones se consideran confirmadas cuando están suficientemente
+enterradas en la blockchain. Por convención se esperan 6 confirmaciones.
+
+Cada nueva confirmación reduce exponencialmente el riesgo de reversión.
+*/
+
+// Bloques huérfanos
+
+/*
+Cuando dos mineros generan un bloque al mismo tiempo, se crea un fork 
+temporal en la cadena. Esto se resuelve cuando se mina el siguiente bloque.
+
+El bloque descartado se conoce como "huérfano". Las transacciones 
+en huérfanos vuelven al pool para ser minadas en otro bloque.
+*/
+
+// Incentivos financieros 
+
+/*
+Los mineros reciben una recompensa de nuevos bitcoins por cada bloque 
+minado, empezando con 50 BTC en 2009 y reduciéndose a la mitad cada 
+210.000 bloques.
+
+También reciben las comisiones de transacción de ese bloque. Esto 
+representa los incentivos financieros para mantener la red.
+*/
+
+// Fin de la traducción
